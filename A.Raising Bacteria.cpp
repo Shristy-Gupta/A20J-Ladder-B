9e3774a118681532eeec3c167fb12815845052e0
Shristy Gupta <shristysgupta@gmail.com>
2021-07-15 15:51:05 +0530

    Create A.Raising Bacteria.cpp

---
+++ A.Raising Bacteria.cpp
@@ -0,0 +1,19 @@
+//A. Raising Bacteria
+#include <iostream>
+#include <utility>
+#include <bitset>
+#include <bits/stdc++.h>
+#include <unordered_map>
+#include <cmath>
+#include <cstdint>
+#include <map>
+using namespace std;
+
+
+int main(){
+
+    int x; cin>> x;
+    bitset<64> ar=x;
+    cout<<ar.count()<<endl;
+    return 0;
+}
